4d4425d3d3eb4c1c40072b2956667a11b3f2cef2
Victoric3 <120360885+Victoric3@users.noreply.github.com>
2023-11-18 21:26:47 +0100

    Update openaiApiKeys.js

--- server/utilities/openaiApiKeys.js
+++ server/utilities/openaiApiKeys.js
@@ -1,64 +1,64 @@
-const OpenAI = require("openai");
+// const OpenAI = require("openai");
 
-const API_KEY1A = "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"
-const API_KEY2A = "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"
+// const API_KEY1A = "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"
+// const API_KEY2A = "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"
 
-const API_KEY1B = "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"
-const API_KEY2B = "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"
-const API_KEY3B = "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"
-const API_KEY4B = "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"
-const API_KEY5B = "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"
+// const API_KEY1B = "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"
+// const API_KEY2B = "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"
+// const API_KEY3B = "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"
+// const API_KEY4B = "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"
+// const API_KEY5B = "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"
 
-const API_KEY1C = "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"
-const API_KEY2C = "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"
-const API_KEY3C = "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"
-const API_KEY4C = "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"
-const API_KEY5C = "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"
-const API_KEY6C = "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"
-const openai = new OpenAI({
-    apiKey: API_KEY1B
-});
-const openai2 = new OpenAI({
-    apiKey: API_KEY2B
-});
-const openai3 = new OpenAI({
-    apiKey: API_KEY3B
-});
-const openai4 = new OpenAI({
-    apiKey: API_KEY4B
-});
-const openai5 = new OpenAI({
-    apiKey: API_KEY5B
-});
-const openai6 = new OpenAI({
-    apiKey: API_KEY1C
-});
-const openai7 = new OpenAI({
-    apiKey: API_KEY2C
-});
-const openai8 = new OpenAI({
-    apiKey: API_KEY3C
-});
-const openai9 = new OpenAI({
-    apiKey: API_KEY4C
-});
-const openai10 = new OpenAI({
-    apiKey: API_KEY5C
-});
-const openai11 = new OpenAI({
-    apiKey: API_KEY6C
-});
+// const API_KEY1C = "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"
+// const API_KEY2C = "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"
+// const API_KEY3C = "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"
+// const API_KEY4C = "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"
+// const API_KEY5C = "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"
+// const API_KEY6C = "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"
+// const openai = new OpenAI({
+//     apiKey: API_KEY1B
+// });
+// const openai2 = new OpenAI({
+//     apiKey: API_KEY2B
+// });
+// const openai3 = new OpenAI({
+//     apiKey: API_KEY3B
+// });
+// const openai4 = new OpenAI({
+//     apiKey: API_KEY4B
+// });
+// const openai5 = new OpenAI({
+//     apiKey: API_KEY5B
+// });
+// const openai6 = new OpenAI({
+//     apiKey: API_KEY1C
+// });
+// const openai7 = new OpenAI({
+//     apiKey: API_KEY2C
+// });
+// const openai8 = new OpenAI({
+//     apiKey: API_KEY3C
+// });
+// const openai9 = new OpenAI({
+//     apiKey: API_KEY4C
+// });
+// const openai10 = new OpenAI({
+//     apiKey: API_KEY5C
+// });
+// const openai11 = new OpenAI({
+//     apiKey: API_KEY6C
+// });
 
-module.exports = {
-    openai, 
-    openai2, 
-    openai3, 
-    openai4, 
-    openai5, 
-    openai6,
-    openai7,
-    openai8, 
-    openai9,
-    openai10,
-    openai11
-}
+// module.exports = {
+//     openai, 
+//     openai2, 
+//     openai3, 
+//     openai4, 
+//     openai5, 
+//     openai6,
+//     openai7,
+//     openai8, 
+//     openai9,
+//     openai10,
+//     openai11
+// }
