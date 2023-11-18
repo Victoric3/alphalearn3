c0bd4e2fa229e8c9fc5930ce57c0a8e4b509a795
Victoric3 <120360885+Victoric3@users.noreply.github.com>
2023-11-18 21:24:14 +0100

    Update passageController.js

--- server/controllers/passageController.js
+++ server/controllers/passageController.js
@@ -1,80 +1,80 @@
-const Passage = require('../models/passageSchema')
-    const createMcqQuestion = async ({
-            examBody,
-            examClass,
-            course,
-            type,
-            passage,
-            questions
-        }) => {
+// const Passage = require('../models/passageSchema')
+//     const createMcqQuestion = async ({
+//             examBody,
+//             examClass,
+//             course,
+//             type,
+//             passage,
+//             questions
+//         }) => {
 
-        try {
-            const newQuestion = new Passage({
-                examBody,
-                examClass,
-                course,
-                type,
-                passage,
-                questions
-            });
+//         try {
+//             const newQuestion = new Passage({
+//                 examBody,
+//                 examClass,
+//                 course,
+//                 type,
+//                 passage,
+//                 questions
+//             });
 
-            await newQuestion.save();
-        } catch (error) {
-            console.error(`Error storing ${examBody} question:`, error);
-        }
-    };                 
+//             await newQuestion.save();
+//         } catch (error) {
+//             console.error(`Error storing ${examBody} question:`, error);
+//         }
+//     };                 
 
-    exports.generateMcqQuestionsPassage = async(message, message2, openai, openai2) => {
+//     exports.generateMcqQuestionsPassage = async(message, message2, openai, openai2) => {
 
-        const chat = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: message, 
-        }
-    ],
-    max_tokens:3800
+//         const chat = await openai.chat.completions.create({
+//       model: 'gpt-3.5-turbo',
+//       messages: [
+//         {
+//           role: 'user',
+//           content: message, 
+//         }
+//     ],
+//     max_tokens:3800
     
-    });
-    const chat2 = await openai2.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        {
-          role: 'user',
-          content: message2, 
-        }
-    ],
-    max_tokens:3800
+//     });
+//     const chat2 = await openai2.chat.completions.create({
+//       model: 'gpt-3.5-turbo',
+//       messages: [
+//         {
+//           role: 'user',
+//           content: message2, 
+//         }
+//     ],
+//     max_tokens:3800
     
-    });
+//     });
     
     
     
-    let answers = JSON.parse(chat.choices[0].message.content);
-    let answers2 = JSON.parse(chat2.choices[0].message.content);
-    // let answers = chat.choices[0].message.content
-    // let answers2 = chat2.choices[0].message.content
-    // console.log(answers, chat.choices[0].message, answers2);
-    const totalAnswers = [
-      answers, 
-      answers2
-    ]
-    console.log(totalAnswers);
-    totalAnswers.forEach(async(answer) => {
-        try{
-        await createMcqQuestion({
-          examBody: answer.examBody,
-          examClass: answer.examClass,
-          course : answer.course,
-          type : answer.type,
-          passage: answer.passage,
-          questions: answer.questions
-        });
-    }catch(e){
-        console.log(e);
-    }
-      });
+//     let answers = JSON.parse(chat.choices[0].message.content);
+//     let answers2 = JSON.parse(chat2.choices[0].message.content);
+//     // let answers = chat.choices[0].message.content
+//     // let answers2 = chat2.choices[0].message.content
+//     // console.log(answers, chat.choices[0].message, answers2);
+//     const totalAnswers = [
+//       answers, 
+//       answers2
+//     ]
+//     console.log(totalAnswers);
+//     totalAnswers.forEach(async(answer) => {
+//         try{
+//         await createMcqQuestion({
+//           examBody: answer.examBody,
+//           examClass: answer.examClass,
+//           course : answer.course,
+//           type : answer.type,
+//           passage: answer.passage,
+//           questions: answer.questions
+//         });
+//     }catch(e){
+//         console.log(e);
+//     }
+//       });
     
-    return totalAnswers
-    }
+//     return totalAnswers
+//     }
